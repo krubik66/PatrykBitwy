1bf450fe2659032cfa4eb4e0243e73d095ba63e6
krubik66 <krubik66@gmail.com>
2022-06-09 22:25:22 +0200

    fractions

--- src/Fractions/Fraction.java
+++ src/Fractions/Fraction.java
@@ -1,5 +1,14 @@
 package Fractions;
 
-public abstract class Fraction {
+import java.util.ArrayList;
 
+public abstract class Fraction {
+    private ArrayList unitList;
+    public Fraction() {
+        this.unitList = new ArrayList();
+    }
+    public abstract void addMelee();
+    public abstract void addRange();
+    public abstract void addSpecial();
+    public abstract int unitsLeft();
 }
--- src/Fractions/Units/Unit.java
+++ src/Fractions/Units/Unit.java
@@ -1,4 +1,9 @@
 package Fractions.Units;
 
 public abstract class Unit {
+    private int hp;
+    private int dmg;
+    private int speed;
+
+
 }
